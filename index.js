100b7afc437fde3f950df6eee977e3b4133dcc7e
Isabela <isabela_cmoreira@hotmail.com>
2022-08-24 00:49:11 -0300

    feat: primary settings

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express, {json} from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
+
+import router from "./routes/index.js";
 
 const app = express();
-dotenv.config();
-app.use(cors());
+
 app.use(json());
+app.use(router);
 
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Servidor em pé na porta ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Mode: ${process.env.MODE || "DEV"}`);
+  console.log(`Server is up on port: ${port}`);
+});
