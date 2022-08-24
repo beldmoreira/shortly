35de3f7d92132d76b19e23b4a906a61e902fe077
Isabela <isabela_cmoreira@hotmail.com>
2022-08-24 12:04:26 -0300

    fix: fixed a typo

--- config/tables.sql
+++ config/tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE "users"(
 "email" TEXT NOT NULL UNIQUE,
 "password" TEXT NOT NULL,
 "createdAt" TIMESTAMP DEFAULT NOW()
-)
+);
 
 CREATE TABLE "urls"(
 "id" SERIAL PRIMARY KEY,
@@ -15,11 +15,11 @@ CREATE TABLE "urls"(
 "visitCount" INTEGER NOT NULL DEFAULT 0,
 "userId" INTEGER REFERENCES users(id),
 "createdAt" TIMESTAMP DEFAULT NOW()
-)
+);
 
 CREATE TABLE "sessions"(
 "id" SERIAL PRIMARY KEY,
 "token" TEXT NOT NULL UNIQUE,
 "userId" INTEGER REFERENCES users(id),
 "createdAt" TIMESTAMP DEFAULT NOW()
-)
+);
